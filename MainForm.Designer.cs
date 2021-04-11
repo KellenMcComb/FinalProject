ce7ba2a1054d36062c32428ec7cd11db7aa51d0e
John Wick <kmvisualstudio@hotmail.com>
2021-04-10 22:35:00 -0400

    Finished tooltips on menustrip items

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -31,28 +31,28 @@ namespace FinalProject
         {
             this.components = new System.ComponentModel.Container();
             this.menuTop = new System.Windows.Forms.MenuStrip();
-            this.statusStrip = new System.Windows.Forms.StatusStrip();
-            this.toolStrip = new System.Windows.Forms.ToolStrip();
-            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuEdit = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuWindow = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileNew = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileSave = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileClose = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit = new System.Windows.Forms.ToolStripMenuItem();
             this.menuEditCut = new System.Windows.Forms.ToolStripMenuItem();
             this.menuEditCopy = new System.Windows.Forms.ToolStripMenuItem();
             this.menuEditPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuWindow = new System.Windows.Forms.ToolStripMenuItem();
             this.menuWindowCascade = new System.Windows.Forms.ToolStripMenuItem();
             this.menuWindowTileVertical = new System.Windows.Forms.ToolStripMenuItem();
             this.menuWindowTileHorizontal = new System.Windows.Forms.ToolStripMenuItem();
             this.menuWindowCarList = new System.Windows.Forms.ToolStripMenuItem();
             this.menuWindowAverageUnits = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.menuHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.menuTop.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -69,6 +69,160 @@ namespace FinalProject
             this.menuTop.TabIndex = 1;
             this.menuTop.Text = "menuStrip1";
             // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFileNew,
+            this.menuFileOpen,
+            this.menuFileSave,
+            this.menuFileSaveAs,
+            this.menuFileClose,
+            this.menuFileExit});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(37, 20);
+            this.menuFile.Text = "&File";
+            // 
+            // menuFileNew
+            // 
+            this.menuFileNew.Name = "menuFileNew";
+            this.menuFileNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuFileNew.Size = new System.Drawing.Size(146, 22);
+            this.menuFileNew.Text = "&New";
+            this.menuFileNew.ToolTipText = "Create a new file";
+            // 
+            // menuFileOpen
+            // 
+            this.menuFileOpen.Name = "menuFileOpen";
+            this.menuFileOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.menuFileOpen.Size = new System.Drawing.Size(146, 22);
+            this.menuFileOpen.Text = "&Open";
+            this.menuFileOpen.ToolTipText = "Open a file";
+            // 
+            // menuFileSave
+            // 
+            this.menuFileSave.Name = "menuFileSave";
+            this.menuFileSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.menuFileSave.Size = new System.Drawing.Size(146, 22);
+            this.menuFileSave.Text = "&Save";
+            this.menuFileSave.ToolTipText = "Save the current file";
+            // 
+            // menuFileSaveAs
+            // 
+            this.menuFileSaveAs.Name = "menuFileSaveAs";
+            this.menuFileSaveAs.Size = new System.Drawing.Size(146, 22);
+            this.menuFileSaveAs.Text = "Save &As";
+            this.menuFileSaveAs.ToolTipText = "Select the save location of the current file";
+            // 
+            // menuFileClose
+            // 
+            this.menuFileClose.Name = "menuFileClose";
+            this.menuFileClose.Size = new System.Drawing.Size(146, 22);
+            this.menuFileClose.Text = "&Close";
+            this.menuFileClose.ToolTipText = "Close currently opened windows";
+            // 
+            // menuFileExit
+            // 
+            this.menuFileExit.Name = "menuFileExit";
+            this.menuFileExit.Size = new System.Drawing.Size(146, 22);
+            this.menuFileExit.Text = "E&xit";
+            this.menuFileExit.ToolTipText = "Exit the application";
+            // 
+            // menuEdit
+            // 
+            this.menuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuEditCut,
+            this.menuEditCopy,
+            this.menuEditPaste});
+            this.menuEdit.Name = "menuEdit";
+            this.menuEdit.Size = new System.Drawing.Size(39, 20);
+            this.menuEdit.Text = "&Edit";
+            // 
+            // menuEditCut
+            // 
+            this.menuEditCut.Name = "menuEditCut";
+            this.menuEditCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.menuEditCut.Size = new System.Drawing.Size(144, 22);
+            this.menuEditCut.Text = "Cu&t";
+            this.menuEditCut.ToolTipText = "Copy selected text to the clipboard and delete it";
+            // 
+            // menuEditCopy
+            // 
+            this.menuEditCopy.Name = "menuEditCopy";
+            this.menuEditCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.menuEditCopy.Size = new System.Drawing.Size(144, 22);
+            this.menuEditCopy.Text = "&Copy";
+            this.menuEditCopy.ToolTipText = "Copy selected text to the clipboard";
+            // 
+            // menuEditPaste
+            // 
+            this.menuEditPaste.Name = "menuEditPaste";
+            this.menuEditPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.menuEditPaste.Size = new System.Drawing.Size(144, 22);
+            this.menuEditPaste.Text = "&Paste";
+            this.menuEditPaste.ToolTipText = "Paste the contents of the clipboard";
+            // 
+            // menuWindow
+            // 
+            this.menuWindow.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuWindowCascade,
+            this.menuWindowTileVertical,
+            this.menuWindowTileHorizontal,
+            this.menuWindowCarList,
+            this.menuWindowAverageUnits});
+            this.menuWindow.Name = "menuWindow";
+            this.menuWindow.Size = new System.Drawing.Size(63, 20);
+            this.menuWindow.Text = "&Window";
+            // 
+            // menuWindowCascade
+            // 
+            this.menuWindowCascade.Name = "menuWindowCascade";
+            this.menuWindowCascade.Size = new System.Drawing.Size(179, 22);
+            this.menuWindowCascade.Text = "&Cascade";
+            this.menuWindowCascade.ToolTipText = "Cascade all currently open windows";
+            // 
+            // menuWindowTileVertical
+            // 
+            this.menuWindowTileVertical.Name = "menuWindowTileVertical";
+            this.menuWindowTileVertical.Size = new System.Drawing.Size(179, 22);
+            this.menuWindowTileVertical.Text = "Tile &Vertical";
+            this.menuWindowTileVertical.ToolTipText = "Tile currently open windows vertically";
+            // 
+            // menuWindowTileHorizontal
+            // 
+            this.menuWindowTileHorizontal.Name = "menuWindowTileHorizontal";
+            this.menuWindowTileHorizontal.Size = new System.Drawing.Size(179, 22);
+            this.menuWindowTileHorizontal.Text = "Tile &Horizontal";
+            this.menuWindowTileHorizontal.ToolTipText = "Tile currently open windows horizontally";
+            // 
+            // menuWindowCarList
+            // 
+            this.menuWindowCarList.Name = "menuWindowCarList";
+            this.menuWindowCarList.Size = new System.Drawing.Size(179, 22);
+            this.menuWindowCarList.Text = "Open Car &List";
+            this.menuWindowCarList.ToolTipText = "Open the Car List window";
+            // 
+            // menuWindowAverageUnits
+            // 
+            this.menuWindowAverageUnits.Name = "menuWindowAverageUnits";
+            this.menuWindowAverageUnits.Size = new System.Drawing.Size(179, 22);
+            this.menuWindowAverageUnits.Text = "Open &Average Units";
+            this.menuWindowAverageUnits.ToolTipText = "Open the Average Units Shipped window";
+            // 
+            // menuHelp
+            // 
+            this.menuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuHelpAbout});
+            this.menuHelp.Name = "menuHelp";
+            this.menuHelp.Size = new System.Drawing.Size(44, 20);
+            this.menuHelp.Text = "&Help";
+            // 
+            // menuHelpAbout
+            // 
+            this.menuHelpAbout.Name = "menuHelpAbout";
+            this.menuHelpAbout.Size = new System.Drawing.Size(107, 22);
+            this.menuHelpAbout.Text = "&About";
+            this.menuHelpAbout.ToolTipText = "Show information about this application";
+            // 
             // statusStrip
             // 
             this.statusStrip.Location = new System.Drawing.Point(0, 592);
@@ -84,145 +238,6 @@ namespace FinalProject
             this.toolStrip.Size = new System.Drawing.Size(817, 25);
             this.toolStrip.TabIndex = 3;
             this.toolStrip.Text = "toolStrip1";
-            // 
-            // menuFile
-            // 
-            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.menuFileNew,
-            this.menuFileOpen,
-            this.menuFileSave,
-            this.menuFileSaveAs,
-            this.menuFileClose,
-            this.menuFileExit});
-            this.menuFile.Name = "menuFile";
-            this.menuFile.Size = new System.Drawing.Size(37, 20);
-            this.menuFile.Text = "&File";
-            // 
-            // menuEdit
-            // 
-            this.menuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.menuEditCut,
-            this.menuEditCopy,
-            this.menuEditPaste});
-            this.menuEdit.Name = "menuEdit";
-            this.menuEdit.Size = new System.Drawing.Size(39, 20);
-            this.menuEdit.Text = "&Edit";
-            // 
-            // menuWindow
-            // 
-            this.menuWindow.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.menuWindowCascade,
-            this.menuWindowTileVertical,
-            this.menuWindowTileHorizontal,
-            this.menuWindowCarList,
-            this.menuWindowAverageUnits});
-            this.menuWindow.Name = "menuWindow";
-            this.menuWindow.Size = new System.Drawing.Size(63, 20);
-            this.menuWindow.Text = "&Window";
-            // 
-            // menuHelp
-            // 
-            this.menuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.menuHelpAbout});
-            this.menuHelp.Name = "menuHelp";
-            this.menuHelp.Size = new System.Drawing.Size(44, 20);
-            this.menuHelp.Text = "&Help";
-            // 
-            // menuFileNew
-            // 
-            this.menuFileNew.Name = "menuFileNew";
-            this.menuFileNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.menuFileNew.Size = new System.Drawing.Size(146, 22);
-            this.menuFileNew.Text = "&New";
-            // 
-            // menuFileOpen
-            // 
-            this.menuFileOpen.Name = "menuFileOpen";
-            this.menuFileOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.menuFileOpen.Size = new System.Drawing.Size(146, 22);
-            this.menuFileOpen.Text = "&Open";
-            // 
-            // menuFileSave
-            // 
-            this.menuFileSave.Name = "menuFileSave";
-            this.menuFileSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.menuFileSave.Size = new System.Drawing.Size(146, 22);
-            this.menuFileSave.Text = "&Save";
-            // 
-            // menuFileSaveAs
-            // 
-            this.menuFileSaveAs.Name = "menuFileSaveAs";
-            this.menuFileSaveAs.Size = new System.Drawing.Size(146, 22);
-            this.menuFileSaveAs.Text = "Save &As";
-            // 
-            // menuFileClose
-            // 
-            this.menuFileClose.Name = "menuFileClose";
-            this.menuFileClose.Size = new System.Drawing.Size(146, 22);
-            this.menuFileClose.Text = "&Close";
-            // 
-            // menuFileExit
-            // 
-            this.menuFileExit.Name = "menuFileExit";
-            this.menuFileExit.Size = new System.Drawing.Size(146, 22);
-            this.menuFileExit.Text = "E&xit";
-            // 
-            // menuEditCut
-            // 
-            this.menuEditCut.Name = "menuEditCut";
-            this.menuEditCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
-            this.menuEditCut.Size = new System.Drawing.Size(144, 22);
-            this.menuEditCut.Text = "Cu&t";
-            // 
-            // menuEditCopy
-            // 
-            this.menuEditCopy.Name = "menuEditCopy";
-            this.menuEditCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
-            this.menuEditCopy.Size = new System.Drawing.Size(144, 22);
-            this.menuEditCopy.Text = "&Copy";
-            // 
-            // menuEditPaste
-            // 
-            this.menuEditPaste.Name = "menuEditPaste";
-            this.menuEditPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
-            this.menuEditPaste.Size = new System.Drawing.Size(144, 22);
-            this.menuEditPaste.Text = "&Paste";
-            // 
-            // menuWindowCascade
-            // 
-            this.menuWindowCascade.Name = "menuWindowCascade";
-            this.menuWindowCascade.Size = new System.Drawing.Size(179, 22);
-            this.menuWindowCascade.Text = "&Cascade";
-            // 
-            // menuWindowTileVertical
-            // 
-            this.menuWindowTileVertical.Name = "menuWindowTileVertical";
-            this.menuWindowTileVertical.Size = new System.Drawing.Size(179, 22);
-            this.menuWindowTileVertical.Text = "Tile &Vertical";
-            // 
-            // menuWindowTileHorizontal
-            // 
-            this.menuWindowTileHorizontal.Name = "menuWindowTileHorizontal";
-            this.menuWindowTileHorizontal.Size = new System.Drawing.Size(179, 22);
-            this.menuWindowTileHorizontal.Text = "Tile &Horizontal";
-            // 
-            // menuWindowCarList
-            // 
-            this.menuWindowCarList.Name = "menuWindowCarList";
-            this.menuWindowCarList.Size = new System.Drawing.Size(179, 22);
-            this.menuWindowCarList.Text = "Open Car &List";
-            // 
-            // menuWindowAverageUnits
-            // 
-            this.menuWindowAverageUnits.Name = "menuWindowAverageUnits";
-            this.menuWindowAverageUnits.Size = new System.Drawing.Size(179, 22);
-            this.menuWindowAverageUnits.Text = "Open &Average Units";
-            // 
-            // menuHelpAbout
-            // 
-            this.menuHelpAbout.Name = "menuHelpAbout";
-            this.menuHelpAbout.Size = new System.Drawing.Size(107, 22);
-            this.menuHelpAbout.Text = "&About";
             // 
             // formMain
             // 
